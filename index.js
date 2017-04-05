90c6bda7f1cc3f7990b132ec5adf3d4f4ca02ebb
Drew Mika <drewmika1@yahoo.com>
2017-04-04 21:21:34 -0400

    work on infrastructure and utilites

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-/*jshint esversion: 6 */
-
-var config = require('./config');
 var app = require('express')();
+var config = require('./config.js');
+var utils = require('./util/index');
 
 var routes = require('./controllers/index');
 
 app.set('view engine', 'pug');
+
+app.use(utils.logger);
+app.use(utils.cookie);
+app.use(utils.compression);
+app.use(utils.jsonParser);
+app.use(utils.urlParser);
 
 if(config.static) {
   app.use(require('express').static('./dist'));
--- models/user.js
+++ models/user.js
@@ -1,5 +1,3 @@
-/*jshint esversion: 6 */
-
 var db = require('database');
 
 exports.get = function(name){
